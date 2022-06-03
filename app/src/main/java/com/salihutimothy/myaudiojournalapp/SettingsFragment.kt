b153f3fd33f5601db9e440ebd501cd8bbae616be
DrTim105 <Salihutimothy52@gmail.com>
2022-06-03 19:28:33 +0100

    Updated settings page

--- app/src/main/java/com/salihutimothy/myaudiojournalapp/SettingsFragment.kt
+++ app/src/main/java/com/salihutimothy/myaudiojournalapp/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.salihutimothy.myaudiojournalapp
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -8,6 +10,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        view.findViewById<Toolbar>(R.id.toolbar)?.let {
+//            // Customize toolbar
+//        }
+
+        val toolbar = view.findViewById<Toolbar>(R.id.settingsToolbar)
 
     }
 }
